045912aace5ce8071af479ab5001c40ffa5f300e
Thomas Famula <famula@leifos.de>
2018-02-27 16:16:09 +0100

    Adding files

---
+++ config_inc.example.php
@@ -0,0 +1,19 @@
+<?php
+$g_hostname               = 'your_servername"';
+$g_db_type                = 'your_db_type';
+$g_database_name          = 'your_db_name';
+$g_db_username            = 'your_db_username';
+$g_db_password            = 'your_db_password';
+
+$g_db_table_prefix          = '';
+$g_db_table_suffix          = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = '';
+
+
+# --- Enum Strings Configuration ---
+$g_severity_enum_string = '10:feature,35:text or tweak,50:minor,60:major,75:crash or block';
+
+?>
---
+++ custom_constants_inc.php
@@ -0,0 +1,7 @@
+<?php
+
+# severity
+define( 'TRIVIAL_TEXT_TWEAK', 35 );
+define( 'CRASH_BLOCK', 75 );
+
+?>
